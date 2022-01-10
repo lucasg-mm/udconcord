60152469db9d331eb2338fceb4b6e865fd105d18
Lucas Miranda <lucasalternativo.lg@gmail.com>
2022-01-10 19:49:38 -0300

    Fixed cert-renew script

--- scripts/renew_certs.sh
+++ scripts/renew_certs.sh
@@ -11,7 +11,7 @@ root_path="$(dirname $(dirname $(realpath $0)))"
 /usr/local/bin/docker-compose -f $root_path/docker-compose.yml down
 
 # renews proxy certificate
-openssl req -newkey rsa:2048 -nodes -keyout $root_path/nginx/my-site.com.key -x509 -days 365 -out nginx/my-site.com.crt < $root_path/scripts/cert-template.txt
+openssl req -newkey rsa:2048 -nodes -keyout $root_path/nginx/my-site.com.key -x509 -days 365 -out $root_path/nginx/my-site.com.crt < $root_path/scripts/cert-template.txt
 
 # starts containers again
 /usr/local/bin/docker-compose -f $root_path/docker-compose.yml up
